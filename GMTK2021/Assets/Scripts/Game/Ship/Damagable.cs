76301ce6c038f9a66298d7f022a44766e66e1d9d
Kyle <kylengelmann@gmail.com>
2021-06-13 00:21:28 -0500

    Fixes some player stuff for inside player, simple damage

--- GMTK2021/Assets/Scripts/Game/Ship/Damagable.cs
+++ GMTK2021/Assets/Scripts/Game/Ship/Damagable.cs
@@ -6,8 +6,13 @@ public class Damagable : MonoBehaviour
 {
     public bool bIsTop;
 
+    public ShipController controller;
+    public Animator animator;
+
     private void OnCollisionEnter(Collision collision)
     {
+        GameManager.ship.OnHit();
+        animator.SetBool("Damage", true);
         Debug.Log("Ow");
     }
 }
